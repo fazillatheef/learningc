462524fa4df15ba9c5189716bbc89059a6281d36
Fazil Abdul Lathif <fazillatheef@gmail.com>
2024-03-24 07:12:11 +0000

    Updated notes and linesort for EOF

--- Chapter05/linesort.c
+++ Chapter05/linesort.c
@@ -9,7 +9,7 @@ int line_count = 0;
 int getlines(char *lineptr[],int);
 void read_line(char [],int);
 void print_lines(char *lineptr[],int lines);
-void str_qsort(char *[],int);
+void str_qsort(char *[],int,int);
 
 int main(int argc, char *argv[])
 {   
@@ -21,8 +21,9 @@ int main(int argc, char *argv[])
         printf("Number of lines is maximum!!!");
     }
     printf("Line count : %d\n",line_count);
-    str_qsort(lineptr,line_count);
+    str_qsort(lineptr,0,line_count -1);
     print_lines(lineptr, line_count);
+    for (int i = 0; i < line_count; i++) free(lineptr[i]);
     return 0;
 }
 
@@ -50,13 +51,28 @@ void read_line(char str[],int line_len)
 {
     char ch;
     int i;
-    for(i = 0; (ch = getchar()) != '\n' && i < line_len - 1; i++){
+    for(i = 0; (ch = getchar()) != '\n' && ch != EOF && i < line_len - 1; i++){
         str[i] = ch;
     }
     str[i] = '\0';
 }
-
-void str_qsort(char *lines[],int line_count)
+void swap(char *v[], int i, int j){
+    char *temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+void str_qsort(char *lines[],int left, int right)
 {
-    return;
+    int i, last;
+    if(left >= right)
+        return;
+    swap(lines,left,(left + right)/2);
+    last = left;
+    for( i = left + 1;i<= right; i++)
+        if (strcmp(lines[i],lines[left]) < 0)
+            swap(lines,++last,i);
+    swap(lines,left,last);
+    str_qsort(lines,left,last);
+    str_qsort(lines,left + 1,right);
 }
